03dbb36795e35f57ddf8f0054414646abe293cdc
NastyaVasenina <amedeo_@mail.ru>
2020-12-13 18:39:15 +0300

    feat: feat closures tasks

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -23,8 +23,11 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => {
+    const result = g(x);
+    return f(result);
+  };
 }
 
 
@@ -44,8 +47,11 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (number) => {
+    const result = number ** exponent;
+    return result;
+  };
 }
 
 
